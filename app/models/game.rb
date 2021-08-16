0cb18387a2f71091e3d2d28751fed231d376f78b
Martial Salgues <github@mar7ius.me>
2021-08-16 17:15:15 +0200

    Revert "Games actions"

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,12 +1,4 @@
 class Game < ApplicationRecord
   has_many_attached :photos
-
-  CATEGORIES = ["pinball", "pool", "arcade game"]
-  
   belongs_to :user
-  has_many :bookings
-  validates :address, :description, :price, :category, presence: true
-  validates :description, length: { minimum: 50 }
-  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
-  validates :category, inclusion: { in: CATEGORIES }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :games do
-    resources :bookings
-  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

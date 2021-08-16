79c9b125604823e415d8b0b4214e6ed72bac3e35
diane orlach <diane@lesateliersdelamarmotte.com>
2021-08-16 16:23:32 +0200

    add validate to the game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,8 @@
 class Game < ApplicationRecord
+  CATEGORIES = ["pinball", "pool", "arcade game"]
   belongs_to :user
+  has_many :bookings
+  validates :address, :description, presence: true
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+  validates :category, inclusion: { in: CATEGORIES }
 end

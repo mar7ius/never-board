38b471a8c77ce5a6dac9ccec4f21f30993604f7b
diane orlach <diane@lesateliersdelamarmotte.com>
2021-08-17 12:02:16 +0200

    added controller games &html

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,50 @@
 class GamesController < ApplicationController
+  before_action :set_game, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @games = policy_scope(Game)
+  end
+
+  def show
+    authorize @game
+  end
+
+  def new
+    authorize @game
+    @games = Game.new
+  end
+
+  def create
+    authorize @game
+    @game = Game.new(game_params)
+    if @game.save
+      redirect_to games_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @game.update(game_params)
+    redirect_to game_path(@game)
+  end
+
+  def destroy
+    @game.destroy
+    redirect_to game_path
+  end
+
+  private
+
+  def set_game
+    @game = Game.find(params[:id])
+
+  end
+
+  def game_params
+    params.require(:game).permit(:category, :address, :description, :price, photos: [])
+  end
 end
---
+++ app/policies/game_policy.rb
@@ -0,0 +1,25 @@
+class GamePolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+      # scope.where.not(user: user).order(created_at: :desc)
+    end
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    return true
+  end
+
+  def update
+    record.user == user
+  end
+
+  def destroy?
+    record.user == user
+  end
+
+end

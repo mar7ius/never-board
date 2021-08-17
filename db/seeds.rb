cd84e8d706a7765d2e1ede91f416f1558d510108
Martial Salgues <github@mar7ius.me>
2021-08-17 12:19:26 +0200

    added games to default user

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,6 +18,31 @@ default.avatar.attach(io: avatar, filename: "image")
 puts " try to save user - #{default}"
 default.save!
 
+puts "add a game to default user"
+
+2.times do
+  game = Game.new(
+    category: Game::CATEGORIES.sample,
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.sentence(word_count: (10..30).to_a.sample),
+    price: (20..200).to_a.sample,
+  )
+  list_url_games = {
+    "pinball" => "https://upload.wikimedia.org/wikipedia/en/f/fc/PinballIndianaJonesOblique.jpg",
+    "pool" => "https://www.jmc-billard.com/3695-thickbox_default/snooker-prince-12ft-acajou.jpg",
+    "arcade game" => "https://upload.wikimedia.org/wikipedia/commons/3/3f/Donkey_Kong_arcade_at_the_QuakeCon_2005.png",
+  }
+  puts "adding image to game.."
+  game_image = URI.open(list_url_games[game.category])
+  game.photos.attach(io: game_image, filename: "images")
+  puts "image added, attributing a user.."
+  game.user = default
+  puts "user attributed, try to save.."
+  game.save!
+end
+
+
+
 puts "default user created, add some random users.."
 puts "creating 10 users..."
 10.times do
@@ -32,6 +57,7 @@ puts "creating 10 users..."
 
   avatar = URI.open(list_url_avatar.sample)
   user.avatar.attach(io: avatar, filename: "image")
+  sleep 10
   puts "user n°#{i} created, try to save..."
   user.save!
   puts "user n°#{i} saved !"
@@ -55,6 +81,7 @@ users = User.all
   puts "adding image to game.."
   game_image = URI.open(list_url_games[game.category])
   game.photos.attach(io: game_image, filename: "images")
+  sleep 10
   puts "image added, attributing a user.."
   game.user = User.all.sample
   puts "user attributed, try to save.."

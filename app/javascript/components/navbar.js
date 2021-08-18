697c081e0af839b365a5828f97f87042a8e94112
Martial Salgues <github@mar7ius.me>
2021-08-18 12:50:03 +0200

    added a link to add a new game with corresponding path

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -2,22 +2,31 @@ const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.navbar-lewagon');
   const banner = document.querySelector('.banner');
   const loginID = document.getElementById('btn-login');
-  const login = document.querySelector('.btn-clear');
+  const linkAddNew = document.getElementById('link-add-new');
+  const btnClear = document.querySelectorAll('.btn-clear');
 
   if (navbar && banner) {
     window.addEventListener('scroll', () => {
       if (window.scrollY >= window.innerHeight) {
         navbar.classList.add('navbar-white');
         navbar.classList.remove('navbar-transparent');
+        linkAddNew.classList.add('btn-black');
+        linkAddNew.classList.remove('btn-white');
         if (loginID) {
-          login.classList.add('btn-black');
-          login.classList.remove('btn-white');
+          btnClear.forEach(btn => {
+            btn.classList.add('btn-black');
+            btn.classList.remove('btn-white');
+          });
         }
       } else {
         if (loginID) {
-          login.classList.remove('btn-black');
-          login.classList.add('btn-white');
+          btnClear.forEach(btn => {
+            btn.classList.remove('btn-black');
+            btn.classList.add('btn-white');
+          });
         }
+        linkAddNew.classList.remove('btn-black');
+        linkAddNew.classList.add('btn-white');
         navbar.classList.remove('navbar-white');
         navbar.classList.add('navbar-transparent');
       }

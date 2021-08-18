f489131bfa64f1f2682762861702adcb6c3ca266
Martial Salgues <github@mar7ius.me>
2021-08-18 10:34:39 +0200

    soft style modifications to uniform

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,11 +1,23 @@
 const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.navbar-lewagon');
-  if (navbar) {
+  const banner = document.querySelector('.banner');
+  const loginID = document.getElementById('btn-login');
+  const login = document.querySelector('.btn-clear');
+
+  if (navbar && banner) {
     window.addEventListener('scroll', () => {
       if (window.scrollY >= window.innerHeight) {
         navbar.classList.add('navbar-white');
         navbar.classList.remove('navbar-transparent');
+        if (loginID) {
+          login.classList.add('btn-black');
+          login.classList.remove('btn-white');
+        }
       } else {
+        if (loginID) {
+          login.classList.remove('btn-black');
+          login.classList.add('btn-white');
+        }
         navbar.classList.remove('navbar-white');
         navbar.classList.add('navbar-transparent');
       }

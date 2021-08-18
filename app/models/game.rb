80b0db9c0894737441eec36e1ee09a220dfb95e3
diane orlach <diane@lesateliersdelamarmotte.com>
2021-08-18 11:48:22 +0200

    move upper case to category's game

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,8 +1,8 @@
 class Game < ApplicationRecord
   has_many_attached :photos
 
-  CATEGORIES = ["pinball", "pool", "arcade game"]
-  
+  CATEGORIES = ["Pinball", "Pool", "Arcade game"]
+
   belongs_to :user
   has_many :bookings
   validates :address, :description, :price, :category, presence: true

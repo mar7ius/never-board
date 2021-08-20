02162802a81f753e9b5f7e5c84ab8de72df5ef11
Martial Salgues <github@mar7ius.me>
2021-08-20 15:23:01 +0200

    change price to integer and changed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,36 @@
-Game.destroy_all
-User.destroy_all
-
 require "faker"
 require "open-uri"
 require "yaml"
+
+address_paris = [
+  "65 Faubourg Saint Honoré Paris",
+  "69 Rue de Turbigo, 75003 Paris",
+  "14 Rue de la Font au Roi, 75011 Paris",
+  "101 Rue Saint-Maur, 75011 Paris",
+  "9 Rue de la Monnaie, 75001 Paris",
+]
+
+address_toulouse = [
+  "1 rue Colette, 31200 Toulouse",
+  "57 rue Alsace Lorraine, 31000 Toulouse",
+  "1 Place Etienne Esquirol, 31000 Toulouse",
+  "10 rue Pargaminieres, 31000 Toulouse",
+]
+
+address_lille = [
+  "216 Rue Léon Gambetta, 59000 Lille",
+  "2 Rue du Faubourg des Postes, 59000 Lille",
+  "75 Rue des Postes, 59000 Lille",
+  "42 Rue Gantois, 59000 Lille",
+]
+
+description_generator = [
+  "The Beatles pinball machine will immerse players in 1960’s Beatlemania and feature nine timeless hit songs. The retro-themed playfield features four flippers, eleven drop targets, eight stand-up targets, multiple skill shots, two opto-spinners, a ball-catching magnet and a magnetic spinning record disc in the center of the playfield.",
+  "Stern Pinball’s Star Wars™ Pin™ is a new stylish, affordable and fun pinball machine designed and engineered for the home.  Stern’s new Star Wars™ Pin™ immerses players in the dynamic Star Wars™ galaxy.  The Pin™ is packed with features that provide players with an irresistible game experience including select speech and footage from the original Star Wars™ trilogy, color-changing playfield inserts, a custom sculpted Death Star, and a custom sculpted and interactive TIE fighter.",
+  "On his unrelenting quest for intergalactic omnipotent power, it’s a race to hunt down and recover the six Infinity Gems before Thanos gets his hands on them.  It’s up to the player to stop Thanos and the Black Order from succeeding in this action-packed pinball experience."
+]
+
+
 
 list_url_avatar = [
   "https://res.cloudinary.com/dyzqasku8/image/upload/v1629191122/avatar_seed_neverboard/avatar_1_wqluot.png",
@@ -19,125 +46,19 @@ default.avatar.attach(io: avatar, filename: "image")
 puts " try to save user - #{default}"
 default.save!
 
-puts "add a game to default user"
-
-michel_games = []
-2.times do
-  game = Game.new(
-    category: Game::CATEGORIES.sample,
-    address: Faker::Address.full_address,
-    description: Faker::Lorem.sentence(word_count: (10..30).to_a.sample),
-    price: (20..200).to_a.sample,
-  )
-  list_url_games = {
-    "pinball" => "https://upload.wikimedia.org/wikipedia/en/f/fc/PinballIndianaJonesOblique.jpg",
-    "pool" => "https://www.jmc-billard.com/3695-thickbox_default/snooker-prince-12ft-acajou.jpg",
-    "arcade game" => "https://upload.wikimedia.org/wikipedia/commons/3/3f/Donkey_Kong_arcade_at_the_QuakeCon_2005.png",
-  }
-  puts "adding image to game.."
-  game_image = URI.open(list_url_games[game.category.downcase])
-  game.photos.attach(io: game_image, filename: "images")
-  puts "image added, attributing a user.."
-  game.user = default
-  puts "user attributed, try to save.."
-  game.save!
-  michel_games << game
-end
+puts "creating default user"
+user2 = User.new(first_name: "toto", last_name: "Fils de Jack", email: "jack@gmail.com", password: "lewagon")
+puts "add a avatar image..."
+avatar = URI.open(list_url_avatar.sample)
+user2.avatar.attach(io: avatar, filename: "image")
+puts " try to save user - #{user2}"
+user2.save!
 
 
-
-puts "default user created, add some random users.."
-puts "creating 10 users..."
-users = []
-i = 1
-10.times do
-  puts "Creating user n°#{i}"
-  user = User.new(
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    password: Faker::Internet.password,
-    email: Faker::Internet.email,
-  )
-
-  avatar = URI.open(list_url_avatar.sample)
-  user.avatar.attach(io: avatar, filename: "image")
-  sleep 3
-  puts "user n°#{i} created, try to save..."
-  user.save!
-  users << user
-  puts "user n°#{i} saved !"
-  i += 1
-end
-
-  games = []
-20.times do
-  game = Game.new(
-    category: Game::CATEGORIES.sample,
-    address: Faker::Address.full_address,
-    description: Faker::Lorem.sentence(word_count: (10..30).to_a.sample),
-    price: (20..200).to_a.sample
-  )
-  list_url_games = {
-    "pinball" => "https://upload.wikimedia.org/wikipedia/en/f/fc/PinballIndianaJonesOblique.jpg",
-    "pool" => "https://www.jmc-billard.com/3695-thickbox_default/snooker-prince-12ft-acajou.jpg",
-    "arcade game" => "https://upload.wikimedia.org/wikipedia/commons/3/3f/Donkey_Kong_arcade_at_the_QuakeCon_2005.png"
-  }
-  puts "adding image to game.."
-  game_image = URI.open(list_url_games[game.category.downcase])
-  game.photos.attach(io: game_image, filename: "images")
-  sleep 3
-  puts "image added, attributing a user.."
-  game.user = users.sample
-  puts "user attributed, try to save.."
-  game.save!
-  games << game
-end
-
-puts "creating bookings.."
-  Booking.create!(
-    start_date: Date.today,
-    end_date: Date.today() + 2,
-    status: 0,
-    user: default,
-    game: games.sample
-  )
-
-  Booking.create!(
-    start_date: Date.today,
-    end_date: Date.today() + 4,
-    status: 1,
-    user: default,
-    game: games.sample
-  )
-
-  Booking.create!(
-    start_date: Date.today() - 1,
-    end_date: Date.today() + 1,
-    status: 2,
-    user: default,
-    game: games.sample
-  )
-
-  Booking.create!(
-    start_date: Date.today,
-    end_date: Date.today() + 2,
-    status: 0,
-    user: users.sample,
-    game: michel_games.sample
-  )
-
-  Booking.create!(
-    start_date: Date.today,
-    end_date: Date.today() + 4,
-    status: 1,
-    user: users.sample,
-    game: michel_games.sample
-  )
-
-  Booking.create!(
-    start_date: Date.today() - 1,
-    end_date: Date.today() + 1,
-    status: 2,
-    user: users.sample,
-    game: michel_games.sample
-  )
+puts "creating default user"
+user5 = User.new(first_name: "Michel", last_name: "Fils de Jack", email: "diane@gmail.com", password: "lewagon")
+puts "add a avatar image..."
+avatar = URI.open(list_url_avatar.sample)
+user5.avatar.attach(io: avatar, filename: "image")
+puts " try to save user - #{user5}"
+user5.save!
